336f900001453dbe98efaf11febf678a502fbe8f
Majd <majd_safi_1@hotmail.com>
2023-03-31 17:10:10 -0700

    fix avatar fetch

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col items-center py-15 dark:text-white max-w-3xl">
-        <img alt="avatar image" class="pb-5" src="../icon/avatar.svg" />
+        <img alt="avatar image" class="pb-5" src="/icon/avatar.svg" />
         <div class="pb-16 text-xl">
             <h1>Welcome👋 to my portfolio webiste!</h1>
             <h1>{{ welcomeInfo }}</h1>
